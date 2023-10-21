64aa7175003d1c70c16b2e5a1b1bb32b4b2ac7da
joaoghg <jherreiragarnica@gmail.com>
2023-10-21 20:16:09 -0300

    após muito sofrimento, as setas estão aos lados

--- res/template/js/main.js
+++ res/template/js/main.js
@@ -16,10 +16,9 @@
 
 	// Products Slick
 	$('.products-slick').each(function() {
-		var $this = $(this),
-				$nav = $this.attr('data-nav');
+        const $this = $(this)
 
-		$this.slick({
+        $this.slick({
 			slidesToShow: 4,
 			slidesToScroll: 1,
 			autoplay: true,
@@ -27,31 +26,32 @@
 			speed: 300,
 			dots: false,
 			arrows: true,
-			appendArrows: $nav ? $nav : false,
-			responsive: [{
-	        breakpoint: 991,
-	        settings: {
-	          slidesToShow: 2,
-	          slidesToScroll: 1,
-	        }
-	      },
-	      {
-	        breakpoint: 480,
-	        settings: {
-	          slidesToShow: 1,
-	          slidesToScroll: 1,
-	        }
-	      },
-	    ]
+			responsive: [
+                {
+                    breakpoint: 991,
+                    settings: {
+                      slidesToShow: 2,
+                      slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                      slidesToShow: 1,
+                      slidesToScroll: 1,
+                      arrows: false
+                    }
+                },
+            ]
 		});
 	});
 
 	// Products Widget Slick
 	$('.products-widget-slick').each(function() {
-		var $this = $(this),
-				$nav = $this.attr('data-nav');
+        const $this = $(this),
+            $nav = $this.attr('data-nav');
 
-		$this.slick({
+        $this.slick({
 			infinite: true,
 			autoplay: true,
 			speed: 300,
